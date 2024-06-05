26a0d29c08926055008569aa3adb3eaec9607137
Jose Rios <Roraj77@hotmail.com>
2024-06-04 19:04:18 -0600

    fix update and delete

--- src/screens/UserDetails.js
+++ src/screens/UserDetails.js
@@ -10,146 +10,161 @@ import {
 import { TextInput } from "react-native-gesture-handler";
 
 import { db } from "../db/testeF";
-
+import { collection, onSnapshot, doc, getDoc, deleteDoc, setDoc } from 'firebase/firestore';
 
 const UserDetails = (props) => { 
-    const initialState = {
-      id: "",
-      name: "",
-      email: "",
-      phone: "",
-    };
-  
-    const [user, setUser] = useState(initialState);
-    const [loading, setLoading] = useState(true);
-  
-    const handleTextChange = (value, prop) => {
-      setUser({ ...user, [prop]: value });
-    };
-  
-    const getUserById = async (id) => {
-      const dbRef = firebase.db.collection("users").doc(id);
-      const doc = await dbRef.get();
-      const user = doc.data();
-      setUser({ ...user, id: doc.id });
+  const initialState = {
+    id: "",
+    name: "",
+    email: "",
+    phone: "",
+  };
+
+  const [user, setUser] = useState(initialState);
+  const [loading, setLoading] = useState(true);
+
+  const handleTextChange = (value, prop) => {
+    setUser({ ...user, [prop]: value });
+  };
+
+  const getUserById = async (id) => {
+    try {
+      const dbRef = doc(db, "users", id);
+      const docSnap = await getDoc(dbRef);
+      if (docSnap.exists()) {
+        const userData = docSnap.data();
+        setUser({ ...userData, id: docSnap.id });
+      } else {
+        console.log("No such document!");
+      }
+    } catch (error) {
+      console.error("Error fetching user data: ", error);
+    } finally {
       setLoading(false);
-    };
-  
-    const deleteUser = async () => {
-      setLoading(true)
-      const dbRef = firebase.db
-        .collection("users")
-        .doc(props.route.params.userId);
-      await dbRef.delete();
-      setLoading(false)
+    }
+  };
+
+  const deleteUser = async () => {
+    try {
+      setLoading(true);
+      const dbRef = doc(db, "users", props.route.params.userId);
+      await deleteDoc(dbRef);
       props.navigation.navigate("UserList");
-    };
-  
-    const openConfirmationAlert = () => {
-      Alert.alert(
-        "Removing the User",
-        "Are you sure?",
-        [
-          { text: "Yes", onPress: () => deleteUser() },
-          { text: "No", onPress: () => console.log("canceled") },
-        ],
-        {
-          cancelable: true,
-        }
-      );
-    };
-  
-    const updateUser = async () => {
-      const userRef = db.collection("users").doc(user.id);
-      await userRef.set({
+    } catch (error) {
+      console.error("Error deleting user: ", error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const openConfirmationAlert = () => {
+    Alert.alert(
+      "Removing the User",
+      "Are you sure?",
+      [
+        { text: "Yes", onPress: () => deleteUser() },
+        { text: "No", onPress: () => console.log("canceled") },
+      ],
+      {
+        cancelable: true,
+      }
+    );
+  };
+
+  const updateUser = async () => {
+    try {
+      const userRef = doc(db, "users", user.id);
+      await setDoc(userRef, {
         name: user.name,
         email: user.email,
         phone: user.phone,
       });
       setUser(initialState);
-      props.navigation.navigate("UsersList");
-    };
-  
-    useEffect(() => {
-      getUserById(props.route.params.userId);
-    }, []);
-  
-    if (loading) {
-      return (
-        <View style={styles.loader}>
-          <ActivityIndicator size="large" color="#9E9E9E" />
-        </View>
-      );
+      props.navigation.navigate("UserList");
+    } catch (error) {
+      console.error("Error updating user: ", error);
     }
-  
-    return (
-      <ScrollView style={styles.container}>
-        <View>
-          <TextInput
-            placeholder="Name"
-            autoCompleteType="username"
-            style={styles.inputGroup}
-            value={user.name}
-            onChangeText={(value) => handleTextChange(value, "name")}
-          />
-        </View>
-        <View>
-          <TextInput
-            autoCompleteType="email"
-            placeholder="Email"
-            style={styles.inputGroup}
-            value={user.email}
-            onChangeText={(value) => handleTextChange(value, "email")}
-          />
-        </View>
-        <View>
-          <TextInput
-            placeholder="Phone"
-            autoCompleteType="tel"
-            style={styles.inputGroup}
-            value={user.phone}
-            onChangeText={(value) => handleTextChange(value, "phone")}
-          />
-        </View>
-        <View style={styles.btn}>
-          <Button
-            title="Delete"
-            onPress={() => openConfirmationAlert()}
-            color="#E37399"
-          />
-        </View>
-        <View>
-          <Button title="Update" onPress={() => updateUser()} color="#19AC52" />
-        </View>
-      </ScrollView>
-    );
   };
-  
-  const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      padding: 35,
-    },
-    loader: {
-      left: 0,
-      right: 0,
-      top: 0,
-      bottom: 0,
-      position: "absolute",
-      alignItems: "center",
-      justifyContent: "center",
-    },
-    inputGroup: {
-      flex: 1,
-      padding: 0,
-      marginBottom: 15,
-      borderBottomWidth: 1,
-      borderBottomColor: "#cccccc",
-    },
-    btn: {
-      marginBottom: 7,
-    },
-  });
-  
-  export default UserDetails;
-  
+
+  useEffect(() => {
+    getUserById(props.route.params.userId);
+  }, []);
+
+  if (loading) {
+    return (
+      <View style={styles.loader}>
+        <ActivityIndicator size="large" color="#9E9E9E" />
+      </View>
+    );
+  }
+
+  return (
+    <ScrollView style={styles.container}>
+      <View>
+        <TextInput
+          placeholder="Name"
+          autoCompleteType="username"
+          style={styles.inputGroup}
+          value={user.name}
+          onChangeText={(value) => handleTextChange(value, "name")}
+        />
+      </View>
+      <View>
+        <TextInput
+          autoCompleteType="email"
+          placeholder="Email"
+          style={styles.inputGroup}
+          value={user.email}
+          onChangeText={(value) => handleTextChange(value, "email")}
+        />
+      </View>
+      <View>
+        <TextInput
+          placeholder="Phone"
+          autoCompleteType="tel"
+          style={styles.inputGroup}
+          value={user.phone}
+          onChangeText={(value) => handleTextChange(value, "phone")}
+        />
+      </View>
+      <View style={styles.btn}>
+        <Button
+          title="Delete"
+          onPress={() => openConfirmationAlert()}
+          color="#E37399"
+        />
+      </View>
+      <View>
+        <Button title="Update" onPress={() => updateUser()} color="#19AC52" />
+      </View>
+    </ScrollView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 35,
+  },
+  loader: {
+    left: 0,
+    right: 0,
+    top: 0,
+    bottom: 0,
+    position: "absolute",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  inputGroup: {
+    flex: 1,
+    padding: 0,
+    marginBottom: 15,
+    borderBottomWidth: 1,
+    borderBottomColor: "#cccccc",
+  },
+  btn: {
+    marginBottom: 7,
+  },
+});
+
+export default UserDetails;
